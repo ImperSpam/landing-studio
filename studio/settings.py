b5a4d0c6f69b9d35a395630bd0a0fec866c7c4cd
ImperSpam <ImperSpam@gmail.com>
2020-03-27 18:11:46 +0300

    add django heroku

--- studio/settings.py
+++ studio/settings.py
@@ -1,5 +1,5 @@
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -113,4 +113,5 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join('static'), )
 
-
+# Activate Django-Heroku.
+django_heroku.settings(locals())
