f5d86b0d3b2275d767a1c82f9164c45686303da5
ImperSpam <ImperSpam@gmail.com>
2020-03-27 19:39:02 +0300

    static

--- studio/settings.py
+++ studio/settings.py
@@ -109,9 +109,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder')
 
 STATICFILES_DIRS = (os.path.join('static'), )
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "static", "media")
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
