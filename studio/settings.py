d67eda31bc9755c4ba94f08f77f810aa6ddc11b9
ImperSpam <ImperSpam@gmail.com>
2020-03-27 19:58:10 +0300

    static

--- studio/settings.py
+++ studio/settings.py
@@ -109,11 +109,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, '/app/staticfiles')
 
-MEDIA_URL = '/media/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+# MEDIA_URL = '/media/'
+#
+# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
