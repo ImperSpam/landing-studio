fed95c4fc06ed0ff431fede6967690a1131d6b34
ImperSpam <ImperSpam@gmail.com>
2020-03-27 18:52:52 +0300

    static

--- studio/settings.py
+++ studio/settings.py
@@ -109,9 +109,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-STATICFILES_DIRS = (os.path.join('static'), )
+# STATICFILES_DIRS = (os.path.join('static'), )
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
