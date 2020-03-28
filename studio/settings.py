724f54a4e2057be37e405de2e1de27fab8207687
ImperSpam <ImperSpam@gmail.com>
2020-03-28 08:17:40 +0300

    jquery fix

--- studio/settings.py
+++ studio/settings.py
@@ -109,15 +109,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join('static'), )
 
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # MEDIA_URL = '/media/'
 #
-# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+# MEDIA_ROOT = os.path.join(BASE_DIR, "static", "media")
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
